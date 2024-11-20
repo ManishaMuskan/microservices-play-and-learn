eb2a69d50ad5d2019e2562b30bf4ebe3892f8bdb
Manisha Sinha <manigetintouch@gmail.com>
2024-11-20 16:36:56 +0530

    client code updated to show comments based on status

--- event-driven-mini-architechture/client/src/ListComments.js
+++ event-driven-mini-architechture/client/src/ListComments.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const ListComments = ({comments}) => {
+const ListComments = ({ comments }) => {
   return (
-    <ul className='list-group my-2'>{comments.map(comment => (
-        <li key={comment.id} className='list-group-item'>{comment.content}</li>
-    ))}</ul>
-  )
-}
+    <ul className='list-group my-2'>
+      {comments.map((comment) => {
+        let content;
 
-export default ListComments
+        if (comment.status === "approved") {
+          content = comment.content;
+        }
+
+        if (comment.status === "pending") {
+          content = "This comment is awaiting moderation";
+        }
+
+        if (comment.status === "rejected") {
+          content = "This comment has been rejected";
+        }
+
+        return (
+          <li key={comment.id} className='list-group-item'>
+            {content}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ListComments;
